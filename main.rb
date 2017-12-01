7e915b9e8eae7121c38ce923492d54e338a65bfc
BrandonRBlank <brandon_blank_r@yahoo.com>
2017-12-01 10:53:50 -0500

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,19 +1,23 @@
 # set :environment, :production
 require 'sinatra'
-require 'bcrypt' # for password encryption
-# require 'data_mapper' # Not used yet, cant get DB to work on my local
-require './database_accessor.rb'
+require 'bcrypt'
+require 'sqlite3'
 
 enable :sessions
 
-# temp array to act as db for my testing
-temp_db = {}
 vote = 0
+
+database = SQLite3::Database.new("database/UserData.database")
+database.execute("CREATE TABLE IF NOT EXISTS UserData (
+                            id STRING PRIMARY KEY,
+                            username STRING UNIQUE,
+                            password STRING,
+                            role INTEGER);")
 
 helpers do
 
   def login?
-    if session[:username].nil? # probably will change session id to an actual id instead of username when db is up
+    if session[:username].nil?
       return false
     else
       return true
@@ -31,29 +35,35 @@ get '/' do
   erb :HomePage
 end
 
+post '/' do
+  File.open('public/uploads/' + params['myFile'][:filename], "w") do |f|
+    f.write(params['myFile'][:tempfile].read)
+  end
+  @uploaded = "File successfully uploaded"
+  erb :HomePage
+end
+
 get '/signup' do
   erb :signup
 end
 
 post '/signup' do
   # More validation will be needed (especially for DB usage)
-  # this only posts username and password to the homepage, we will need to store in db
   if params[:username].to_s.empty? or params[:password].to_s.empty?
     @error = "Username or Password have to be filled"
     erb :signup
   else
     pw_salt = BCrypt::Engine.generate_salt
     pw_hash = BCrypt::Engine.hash_secret(params[:password], pw_salt)
+  end
 
-    temp_db[params[:username]] = {
-        :salt => pw_salt,
-        :hash => pw_hash
-    }
-
-    print temp_db
-
+  begin
+    database.execute("INSERT INTO UserData VALUES(?,?,?,?)", pw_salt, params[:username], pw_hash, "0")
+  rescue SQLite3::ConstraintException
+    @error = "Username already taken"
+    erb :signup
+  else
     @user = params[:username]
-    @password = pw_hash
     session[:username] = params[:username]
 
     erb :HomePage
@@ -65,20 +75,25 @@ get '/login' do
 end
 
 post '/login' do
+  name = database.execute("SELECT username FROM UserData WHERE username=?", params[:username])
   if params[:username].to_s.empty? or params[:password].to_s.empty?
     @error = "Enter Username and Password"
     erb :login
-  else if temp_db.has_key?(params[:username])
-         user = temp_db[params[:username]]
-         if user[:hash] == BCrypt::Engine.hash_secret(params[:password], user[:salt])
-           session[:username] = params[:username]
-           @user = params[:username]
-           erb :HomePage
+    else if name.empty?
+           @error = "Incorrect Username or Password"
+           erb :login
+         else
+           pw_db = database.execute("SELECT password FROM UserData WHERE username=?", params[:username])
+           salt_db = database.execute("SELECT id FROM UserData WHERE username=?", params[:username])
+           if pw_db[0][0] == BCrypt::Engine.hash_secret(params[:password], salt_db[0][0])
+             session[:username] = params[:username]
+             @user = params[:username]
+             erb :HomePage
+           else
+             @error = "Incorrect Username or Password"
+             erb :login
+           end
          end
-       else
-         @error = "Incorrect Username or Password"
-         erb :login
-       end
   end
 end
 
@@ -88,9 +103,14 @@ get '/logout' do
 end
 
 get '/submissions' do
-  @item = "test"
-  @value = 0
-  erb :Vote
+  if session[:username].nil?
+    @error = "You need to sign up to view submissions"
+    erb :signup
+  else
+    @item = "test"
+    @value = 0
+    erb :Vote
+  end
 end
 
 post '/submissions' do
