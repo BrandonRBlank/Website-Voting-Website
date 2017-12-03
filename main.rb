46b5358b8fcc47ec20937b214f5cc6ee74a1d67f
BrandonRBlank <brandon_blank_r@yahoo.com>
2017-12-02 20:25:23 -0500

    Add files via upload

--- main.rb
+++ main.rb
@@ -5,11 +5,13 @@ require 'sqlite3'
 require 'zip'
 require 'csv'
 require 'sequel'
-# require 'FileUtils'
+require 'fileutils'
 
 enable :sessions
 
-vote = 0
+fsub = {}
+pos = Array.new
+$first = true
 
 database = SQLite3::Database.new("database/UserData.database")
 database.execute("CREATE TABLE IF NOT EXISTS UserData (
@@ -17,7 +19,9 @@ database.execute("CREATE TABLE IF NOT EXISTS UserData (
                             username    STRING UNIQUE,
                             password    STRING,
                             role        INTEGER,
-                            voteID      STRING);")
+                            vote1       STRING,
+                            vote2       STRING,
+                            vote3       STRING);")
 
 helpers do
 
@@ -37,9 +41,24 @@ helpers do
     end
   end
 
-  def not_voted?
-    print session[:voted]
-    if session[:voted] == "nil"
+  def not_voted1?
+    if session[:voted1] == "nil"
+      return true
+    else
+      return false
+    end
+  end
+
+  def not_voted2?
+    if session[:voted2] == "nil"
+      return true
+    else
+      return false
+    end
+  end
+
+  def not_voted3?
+    if session[:voted3] == "nil"
       return true
     else
       return false
@@ -66,7 +85,6 @@ get '/signup' do
 end
 
 post '/signup' do
-  # More validation will be needed (especially for DB usage)
   if params[:username].to_s.empty? or params[:password].to_s.empty?
     @error = "Username or Password have to be filled"
     erb :signup
@@ -82,7 +100,7 @@ post '/signup' do
   end
 
   begin
-    database.execute("INSERT INTO UserData VALUES(?,?,?,?,?)", pw_salt, params[:username], pw_hash, role, "nil")
+    database.execute("INSERT INTO UserData VALUES(?,?,?,?,?,?,?)", pw_salt, params[:username], pw_hash, role, "nil", "nil", "nil")
   rescue SQLite3::ConstraintException
     @error = "Username already taken"
     erb :signup
@@ -90,7 +108,9 @@ post '/signup' do
     @user = params[:username]
     session[:username] = params[:username]
     session[:id] = role
-    session[:voted] = "nil"
+    session[:voted1] = "nil"
+    session[:voted2] = "nil"
+    session[:voted3] = "nil"
 
     erb :HomePage
   end
@@ -112,11 +132,13 @@ post '/login' do
            pw_db = database.execute("SELECT password FROM UserData WHERE username=?", params[:username])
            salt_db = database.execute("SELECT id FROM UserData WHERE username=?", params[:username])
            role = database.execute("SELECT role FROM UserData WHERE username=?", params[:username])
-           vote = database.execute("SELECT voteID FROM UserData WHERE username=?", params[:username])
+           vote = database.execute("SELECT vote1,vote2,vote3 FROM UserData WHERE username=?", params[:username])
            if pw_db[0][0] == BCrypt::Engine.hash_secret(params[:password], salt_db[0][0])
              session[:username] = params[:username]
              session[:id] = role[0][0]
-             session[:voted] = vote
+             session[:voted1] = vote[0][0]
+             session[:voted2] = vote[0][1]
+             session[:voted3] = vote[0][2]
              @user = params[:username]
              erb :HomePage
            else
@@ -130,36 +152,92 @@ end
 get '/logout' do
   session[:username] = nil
   session[:id] = nil
+  session[:voted1] = nil
+  session[:voted2] = nil
+  session[:voted3] = nil
   redirect '/'
 end
 
 get '/submissions' do
+  i = 0
+  begin
+    Dir.chdir('public/content/')
+    Dir.glob('*').select do |f|
+      File.directory? f
+      fsub[i] = {
+          :id => f,
+          :score => 0
+      }
+      pos << i
+      i += 1
+    end
+  rescue
+      puts ""
+  end
+  order = 1 + rand(fsub.length) # try to randomize, not working yet
+
   if session[:username].nil?
     @error = "You need to sign up to view submissions"
     erb :signup
-  else
-    @item = "test"
-    @value = 0
-    @voteID = "TEST ID NEED TO BE RANDOM" # needs to be random/sequential for each website
+  else if $first
+    $displayArr = Array.new(i) { Array.new(4) }
+    i -= 1
+    while i != -1 do
+      $displayArr[i][0] = "view/" + fsub[i].values[0].to_s
+      $displayArr[i][1] = fsub[i].values[0]
+      $displayArr[i][2] = fsub[i].values[1]
+      i -= 1
+    end
+    $first = false
+    @item = $displayArr
     erb :Vote
+  else
+    @item = $displayArr
+    erb :Vote
+       end
   end
 end
 
 post '/submissions' do
-  if params.keys[0] == 'up'
-    vote = vote + 1
-    database.execute("UPDATE UserData SET voteID=? WHERE username=?", params[:up], session[:username])
-    session[:voted] = params[:up]
+  if params.values[0] == '1'
+    database.execute("UPDATE UserData SET vote1=? WHERE username=?", params[:name], session[:username])
+    session[:voted1] = params[:name]
+    row = $displayArr.detect{|aa| aa.include?(params[:name].to_s)}
+    pair =  [row.index(params[:name]), $displayArr.index(row)]
+
+    if fsub.values[pair[1]][:id] == params[:name]
+      fsub.values[pair[1]][:score] += 3
+      $displayArr[pair[1]][2] += 3
+    end
   end
-  @value = vote
-  @item = "test"
-  @voteID = "TEST ID NEED TO BE RANDOM"
+  if params.values[0] == '2'
+    database.execute("UPDATE UserData SET vote2=? WHERE username=?", params[:name], session[:username])
+    session[:voted2] = params[:name]
+    row = $displayArr.detect{|aa| aa.include?(params[:name].to_s)}
+    pair =  [row.index(params[:name]), $displayArr.index(row)]
+
+    if fsub.values[pair[1]][:id] == params[:name]
+      fsub.values[pair[1]][:score] += 2
+      $displayArr[pair[1]][2] += 2
+    end
+  end
+  if params.values[0] == '3'
+    database.execute("UPDATE UserData SET vote3=? WHERE username=?", params[:name], session[:username])
+    session[:voted3] = params[:name]
+    row = $displayArr.detect{|aa| aa.include?(params[:name].to_s)}
+    pair =  [row.index(params[:name]), $displayArr.index(row)]
+
+    if fsub.values[pair[1]][:id] == params[:name]
+      fsub.values[pair[1]][:score] += 1
+      $displayArr[pair[1]][2] += 1
+    end
+  end
+  @item = $displayArr
   erb :Vote
 end
 
-get '/test' do
-  # send_file File.read('\content\bootstrap project\bootstrap.html')
-  send_file 'public/content/bootstrap project/bootstrap.html'
+get '/view/:id' do |id|
+  send_file "public/content/#{id}/index.html"
 end
 
 get '/upload' do
@@ -167,56 +245,67 @@ get '/upload' do
 end
 
 post '/upload' do
-  if params['myFile'][:type] == "application/vnd.ms-excel"
-    fname = 'public/uploads/CSV/' + params['myFile'][:filename]
+  begin
+    if params['myFile'][:type] == "application/vnd.ms-excel"
+      fname = 'public/uploads/CSV/' + params['myFile'][:filename]
 
-    File.open(fname, "w") do |f|
-      f.write(params['myFile'][:tempfile].read)
+      File.open(fname, "w") do |f|
+        f.write(params['myFile'][:tempfile].read)
+      end
+
+      # CSV Row Format:  strArr[i] => ['Username', 'Password', 'Role #']
+      strArr = CSV.read fname
+
+      database.execute("DELETE FROM Userdata")
+
+      # Add all elements from CSV to Database. Make sure that .csv file does not
+      # Contain blank spaces!
+      strArr.each do |s|
+        pw_salt = BCrypt::Engine.generate_salt
+        pw_hash = BCrypt::Engine.hash_secret(s[1], pw_salt)
+        database.execute("INSERT INTO UserData VALUES(?,?,?,?,?,?,?)", pw_salt,
+                         s[0], pw_hash, s[2], "nil", "nil", "nil")
+      end
+
+      @uploaded = "CSV File Successfully Uploaded"
     end
-    # BEGIN CODING CHANGES -----------------------------------------------------
-    # CSV Row Format:  strArr[i] => ['Username', 'Password', 'Role #']
+    if params['myFile'][:type] == "application/octet-stream"
+      corrupt = true
+      File.open('public/uploads/ZIP/' + params['myFile'][:filename], "w") do |f|
+        f.write(params['myFile'][:tempfile].read)
+      end
 
-    strArr = CSV.read fname
-
-    database.execute("DELETE FROM Userdata")
-
-    # Add all elements from CSV to Database. Make sure that .csv file does not
-    # Contain blank spaces!
-    strArr.each do |s|
-      pw_salt = BCrypt::Engine.generate_salt
-      pw_hash = BCrypt::Engine.hash_secret(s[1], pw_salt)
-      database.execute("INSERT INTO UserData VALUES(?,?,?,?,?)", pw_salt,
-                       s[0], pw_hash, s[2], "nil")
-    end
-    # END CODING CHANGES -------------------------------------------------------
-
-    @uploaded = "CSV File Successfully Uploaded"
-  end
-  if params['myFile'][:type] == "application/octet-stream"
-    File.open('public/uploads/ZIP/' + params['myFile'][:filename], "w") do |f|
-      f.write(params['myFile'][:tempfile].read)
-    end
-
-    Zip::File.open('public/uploads/ZIP/' + params['myFile'][:filename]) { |zip_file|
-      zip_file.each { |f|
-        f_path=File.join("public/content", f.name)
-        FileUtils.mkdir_p(File.dirname(f_path))
-        zip_file.extract(f, f_path) unless File.exist?(f_path)
+      Zip::File.open('public/uploads/ZIP/' + params['myFile'][:filename]) { |zip_file|
+        zip_file.each { |f|
+          f_path = File.join('public/content', f.name)
+          FileUtils.mkdir_p(File.dirname(f_path))
+          zip_file.extract(f, f_path) unless File.exist?(f_path)
+          corrupt = false
+        }
       }
-    }
-    @uploaded = "ZIP File Successfully Uploaded"
+      if corrupt
+        @uploaded = "ZIP File Corrupt"
+      else
+        @uploaded = "ZIP File Successfully Uploaded"
+      end
+    else
+      @uploaded = "No File"
+    end
+  rescue
+    @uploaded = "Upload Valid File Type (.csv or .zip)"
   end
   erb :upload
 end
 
 get '/report' do
-  @arr = database.execute("SELECT username,voteID FROM UserData WHERE role NOT LIKE 2")
+  @arr = database.execute("SELECT username,vote1,vote2,vote3 FROM UserData WHERE role NOT LIKE 2")
   erb :report
 end
 
 post '/report' do
   items = database.execute("SELECT username,password,role FROM UserData")
-  File.open('public/uploads/CSV/data.csv', 'w') do |f|
+  FileUtils.mkdir_p('/uploads/CSV/') unless File.exists?('/uploads/CSV/')
+  File.open(File.join('public/uploads/CSV/', 'data.csv'), 'w') do |f|
     items.each do |data|
       f << data.to_csv(:force_quotes => true, :skip_blanks => true).gsub('\r\n', '\n')
     end
